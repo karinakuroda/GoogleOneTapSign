f7c39c93cbc89705de23b1402be8fbecce03b0df
Karina Midori Kuroda <kmidori@viajanetsp.local>
2018-02-12 14:06:28 -0200

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts('https://static.shopback.net/tags/service-worker.js?nocache=1');
+// importScripts('https://static.shopback.net/tags/service-worker.js?nocache=1');
 
 
 importScripts('/cache-polyfill.js');
@@ -6,6 +6,7 @@ importScripts('/cache-polyfill.js');
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('airhorner').then(function(cache) {
+	   console.log('install ok');
      return cache.addAll([
        '/',
        '/index.html'
