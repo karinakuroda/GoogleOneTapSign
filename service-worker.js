44e47c03550522e48b29a770b509c794d827205e
Karina Midori Kuroda <kmidori@viajanetsp.local>
2018-02-12 14:29:30 -0200

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,16 @@
-
+var CACHE_NAME = 'my-account-cache';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+			
 self.addEventListener('install', function(e) {
+	// Perform install steps
 	e.waitUntil(
-		caches.open('airhorner').then(function(cache) {
-			console.log('install ok');
-		   
-			var CACHE_NAME = 'my-account-cache';
-			var urlsToCache = [
-			  '/',
-			  '/index.html'
-			];
-
-			self.addEventListener('install', function(event) {
-			  // Perform install steps
-			  event.waitUntil(
-				caches.open(CACHE_NAME)
-				  .then(function(cache) {
-					console.log('Opened cache');
-					return cache.addAll(urlsToCache);
-				  })
-			  );
-			});
-	   })
- );
+	caches.open(CACHE_NAME)
+	  .then(function(cache) {
+		console.log('Opened cache');
+		return cache.addAll(urlsToCache);
+	  })
+	);
 });
