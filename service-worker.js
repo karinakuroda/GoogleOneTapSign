a594fea2282b432d3c41c5252c318bcb26bb756d
Karina Midori Kuroda <kmidori@viajanetsp.local>
2018-02-12 13:49:51 -0200

    sw

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,15 @@
 importScripts('https://static.shopback.net/tags/service-worker.js?nocache=1');
+
+
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html'
+     ]);
+   })
+ );
+});
