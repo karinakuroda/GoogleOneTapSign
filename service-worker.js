8d81c58c1cfaa1d3113c1a75a723ccf2501f70e3
Karina Midori Kuroda <kmidori@viajanetsp.local>
2018-02-12 14:25:55 -0200

    sw]

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,25 @@
-// importScripts('https://static.shopback.net/tags/service-worker.js?nocache=1');
-
-
-// importScripts('/cache-polyfill.js');
 
 self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-	   console.log('install ok');
-     return cache.addAll([
-       '/',
-       '/index.html'
-     ]);
-   })
+	e.waitUntil(
+		caches.open('airhorner').then(function(cache) {
+			console.log('install ok');
+		   
+			var CACHE_NAME = 'my-account-cache';
+			var urlsToCache = [
+			  '/',
+			  '/index.html'
+			];
+
+			self.addEventListener('install', function(event) {
+			  // Perform install steps
+			  event.waitUntil(
+				caches.open(CACHE_NAME)
+				  .then(function(cache) {
+					console.log('Opened cache');
+					return cache.addAll(urlsToCache);
+				  })
+			  );
+			});
+	   })
  );
 });
